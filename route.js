6c9b05252b07dc6adca2c8bfb1c7cbab8ddecabb
hanherb <dyo.pyon@gmail.com>
2020-03-04 10:39:31 +0700

    scraping basic needs

--- route.js
+++ route.js
@@ -6,8 +6,16 @@ const router = express.Router();
 
 router.route('/').get(function(req, res) {rf.index(req, res)});
 
+router.route('/login').get(function(req, res) {rf.login(req, res)});
+
+router.route('/session').get(function(req, res) {rf.session(req, res)});
+
 router.route('/admin').get(function(req, res) {rf.admin(req, res)});
 
-router.route('/send-otp').get(function(req, res) {rf.send(req, res)});
+router.route('/get-otp').get(function(req, res) {rf.getOtp(req, res)});
+
+router.route('/post-otp').post(function(req, res) {rf.postOtp(req, res)});
+
+router.route('/initiate-lock').post(function(req, res) {rf.initiateLock(req, res)});
 
 module.exports = router;
--- src/route-function.js
+++ src/route-function.js
@@ -1,28 +1,90 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const db = require('./db-connect.js');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 // const SendOtp = require('sendotp');
 
+app.use(cookieParser());
+
+const sess = {
+	secret: 'hehe',
+	cookie: {}
+}
+
+app.use(session(sess));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 exports.index = function(req, res) {
-	console.log("Works");
 	res.json("Works");
+}
+
+exports.login = function(req, res) {
+	var username = "dyo";
+	var password = "123";
+
+	db.query("SELECT * FROM users WHERE username = '"+username+"' AND password = '"+password+"'", function(result) {
+		if(result.length) {
+			session.username = result[0].username;
+			console.log("Welcome " + session.username);
+			res.json("Welcome " + session.username);
+		}
+		else {
+			console.log(404);
+			res.json(404);
+		}
+	});
+}
+
+exports.session = function(req, res) {
+	res.json(session.username);
 }
 
 exports.admin = function(req, res) {
 	res.sendFile(path.join(__dirname, '../public', 'admin.html'));
 }
 
-exports.adminOtp = function(req, res) {
-	// const sendOtp = new SendOtp('1111');
-
+exports.getOtp = function(req, res) {
 	var otp =  Math.floor(1000 + Math.random() * 9000);
 
-	res.json(otp); // --Next, store in db with active flag. After a certain amount of time, change flag to inactive
+	db.query("INSERT INTO otp (id, otp_code, status, user_id) VALUES('', '"+otp+"', 1, 2)");
+	//status 0 = inactive, 1 = active(default), 2 = finished
+
+	res.json(otp);
 
 	// --Send the otp to admin's phone number
 
+	// const sendOtp = new SendOtp('1111');
 	// sendOtp.send("+6282299392596", "PRIIND", function (error, data) {
 	// 	console.log(data);
 	// 	res.json(data);
 	// });
+}
+
+exports.postOtp = function(req, res) {
+	var username = req.body.username;
+	var password = req.body.password;
+	var otp = req.body.otp;
+
+	db.query("SELECT * FROM users WHERE username = '"+username+"' AND password = '"+password+"'", function(result) {
+		if(result.length) {
+			var user_id = result[0].id;
+		}
+		else {
+			console.log(404);
+		}
+	});
+
+	var dbOtp = db.query("SELECT * FROM otp WHERE user_id = "+user_id);
+
+	if(otp == dbOtp) {
+		res.json(1);
+	}
+	else {
+		res.json(403);
+	}
 }
